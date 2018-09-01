6dae76307cd2ba1c95f0796eb2ac173bf5ff57ef
tz309806 <codebackup6422@gmail.com>
2018-09-01 15:13:06 -0400

    second commit for db con

--- src/main/java/ActorRowMapper.java
+++ src/main/java/ActorRowMapper.java
@@ -1,13 +1,16 @@
 import java.sql.ResultSet;
 import java.sql.SQLException;
+
+import main.models.ActorTableSample;
 import org.springframework.jdbc.core.RowMapper;
 public class ActorRowMapper implements RowMapper<ActorTableSample> {
     @Override
     public ActorTableSample mapRow(ResultSet row, int rowNum) throws SQLException {
         ActorTableSample actor = new ActorTableSample();
-        actor.setArticleId(row.getInt("articleId"));
-        actor.setTitle(row.getString("title"));
-        actor.setCategory(row.getString("category"));
+        actor.setActorId(row.getInt("actor_id"));
+        actor.setFirstName(row.getString("first_name"));
+        actor.setLastName(row.getString("last_name"));
+        actor.setLastUpdated(row.getDate("last_updated"));
         return actor;
     }
 }
